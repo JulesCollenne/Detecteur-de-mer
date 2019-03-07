4291ef1165a7f530b78ec4ea031764c27e46e3aa
TimotheePiau <timothee.piau@gmail.com>
2019-03-07 15:23:14 +0100

    Correction Reader.py

--- Reader.py
+++ Reader.py
@@ -1,15 +1,19 @@
 from os import listdir
 from os.path import isfile, join
 import Histogramme as Hi
+import numpy
 
 def dataHistogramme(dirPath):
     
-    seaPath = dirPath+"/Mer/"
-    otherPath = dirPath+"/Ailleurs/"
     data=[]
     target=[]
+    
+    seaPath = dirPath+"/Mer/"
+    otherPath = dirPath+"/Ailleurs/"
+    
     seaFileList = listdir(seaPath)
     otherFileList = listdir(otherPath)
+    
     for iSea in range(0,len(seaFileList)-1):
         data.append(Hi.VectorHistogrammeC(seaFileList[iSea]))
         target.append(1)
@@ -18,5 +22,8 @@ def dataHistogramme(dirPath):
         data.append(Hi.VectorHistogrammeC(otherFileList[iOther]))
         target.append(-1)
 
+    numpy.asarray(data)
+    numpy.asarray(target)
+    
     return data,target
 
