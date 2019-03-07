655b8cbe9f5ec6e1f748aa6e07b019752685c3e4
PUPIER Adrien <p14004313@c-sc-s303-17.salsa.univ-amu.fr>
2019-03-07 15:48:11 +0100

    working

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import argparse
+import Reader as re
+from sklearn.model_selection import train_test_split # version 0.18.1
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+
 
 ap=argparse.ArgumentParser()
 ap.add_argument("-t","--train",required= False, help="Path to Data Folder")
@@ -10,15 +15,24 @@ args=vars(ap.parse_args())
 pathTrain=args["train"]
 pathPredict=args["predict"]
 
-print(pathTrain)
-print(pathPredict)
-
-
-
-
 if(pathTrain is not None):
-    pass
+    data,target =re.dataHistogramme(pathTrain)
+    data_test = train_test_split(data, target
+                                 , random_state=0
+                                 , train_size=0.5)
+    data_train, data_test, target_train, target_test = data_test
+    #print(data_train)
+    #print(target_train.shape)
+    
+    #train
+    clf = GaussianNB()
+    clf.fit(data_train, target_train)
+    #predict
+    result = clf.predict(data_test)
+    #score
+    print(accuracy_score(result, target_test))
     #train() to do
+    
 if(pathPredict is not None):
     pass
     #predict() to do
