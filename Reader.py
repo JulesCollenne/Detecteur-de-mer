15bb55caf1421b9e2a93abeb8bfc9dd4d2104229
killian05000 <killianwolfger@hotmail.fr>
2019-03-14 14:42:01 +0100

    Adding ColorRates and fixing Histogramme

--- Reader.py
+++ Reader.py
@@ -2,6 +2,7 @@ from os import listdir
 from os.path import isfile, join
 import Images.Histogramme as Hi
 import numpy
+import Images.ColorRates as Cr
 from PIL import Image
 
 def dataHistogramme(dirPath):
@@ -16,13 +17,13 @@ def dataHistogramme(dirPath):
     otherFileList = listdir(otherPath)
     print(len(seaFileList))
     for iSea in range(0,len(seaFileList)-1):
-        img_path = "../" + seaPath + seaFileList[iSea]
-        img = Image.open(img_path)
-        data.append(Hi.VectorHistogrammeC(img_path)/img.size)
+        img_path = ""+ seaPath + seaFileList[iSea]
+        data.append(Hi.VectorHistogrammeC(img_path))
         target.append(1)
 
     for iOther in range(0,len(otherFileList)-1):
-        data.append(Hi.VectorHistogrammeC("../"+otherPath+otherFileList[iOther]))
+        img_path = ""+otherPath+otherFileList[iOther]
+        data.append(Hi.VectorHistogrammeC(img_path))
         target.append(-1)
 
     data=numpy.asarray(data)
@@ -31,3 +32,33 @@ def dataHistogramme(dirPath):
     print(target.shape)
 
     return data,target
+
+
+def dataColorRates(dirPath):
+    
+    data=[]
+    target=[]
+    
+    seaPath = dirPath+"/Mer/"
+    otherPath = dirPath+"/Ailleurs/"
+    
+    seaFileList = listdir(seaPath)
+    otherFileList = listdir(otherPath)
+    print(len(seaFileList))
+    for iSea in range(0,len(seaFileList)-1):
+        img_path = ""+ seaPath + seaFileList[iSea]
+        data.append(Cr.getColorRates(img_path))
+        target.append(1)
+
+    for iOther in range(0,len(otherFileList)-1):
+        img_path = ""+otherPath+otherFileList[iOther]
+        data.append(Cr.getColorRates(img_path))
+        target.append(-1)
+
+    data=numpy.asarray(data)
+    target=numpy.asarray(target)
+    print(data.shape)
+    print(target.shape)
+
+    return data,target
+
