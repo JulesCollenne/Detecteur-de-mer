c436540b5fdd769e475352f4b84d37c581d95147
killian05000 <killianwolfger@hotmail.fr>
2019-03-21 15:51:58 +0100

    configuring the -p launch

--- main.py
+++ main.py
@@ -2,32 +2,34 @@
 # -*- coding: utf-8 -*-
 import argparse
 import Reader as re
-import Algorithm.bayes as alg
-import Algorithm.ada_boost as alg2
+import Algorithm.bayes as bayes
+import Algorithm.ada_boost as ada
+import Algorithm.Model as model
+
 
 
 ap=argparse.ArgumentParser()
-ap.add_argument("-t","--train",required= False, help="Path to Data Folder")
+ap.add_argument("-f","--fit",required= False, help="Path to Data Folder")
 ap.add_argument("-p","--predict",required=False, help="Path to Data Folder")
 args=vars(ap.parse_args())
 
-pathTrain=args["train"]
+pathTrain=args["fit"]
 pathPredict=args["predict"]
 
-accuracy = 0
-if(pathTrain is not None):
-    
-    for _ in range(20):  
-        data,target =re.dataHistogramme(pathTrain)
-        #print(alg.Bayses(data,target))
-        accuracy += alg.Bayses(data,target)
-        #print(alg2.ada_boost(data, target))
-        #accuracy += alg2.ada_boost(data, target)
-    print("acc = ", accuracy/20)
+accuracy_bayes = 0
+accuracy_ada = 0
+
+if(pathTrain is not None):    
+    data,target =re.dataHistogramme(pathTrain)
+    for _ in range(20):          
+        accuracy_bayes += bayes.Bayes(data,target)        
+        accuracy_ada += ada.ada_boost(data, target)
+    print("Bayses = ", accuracy_bayes/20)
+    print("AdaBoost = ", accuracy_ada/20)
     
     
     
 if(pathPredict is not None):
-    pass
-    #predict() to do
+    print("Bayses loaded = "+str(model.load_Model('Bayes.sav', pathPredict)))
+    print("AdaBoost loaded = "+str(model.load_Model('AdaBoost.sav', pathPredict)))
     
