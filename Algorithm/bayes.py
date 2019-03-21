c1d9c7c716dbd9d928431bb8f9bb3e2737116328
WOLFGER Killian <w16003485@c-sc-s304-06.salsa.univ-amu.fr>
2019-03-21 15:50:44 +0100

    save the generated model

--- Algorithm/bayes.py
+++ Algorithm/bayes.py
@@ -1,19 +1,18 @@
 from sklearn.model_selection import train_test_split # version 0.18.1
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
-def Bayses(data, target):
-# split the data with 50% in each set
-    data_test = train_test_split(data, target
-                                 , random_state=0
-                                 , train_size=0.8)
+from Algorithm import Model as model
 
 
+def Bayes(data, target):
+    data_test = train_test_split(data, target, train_size=0.8, test_size=0.2)
     data_train, data_test, target_train, target_test = data_test
-
-#train
+    #train
     clf = GaussianNB()
     clf.fit(data_train, target_train)
-#predict
+    model.save_Model('Bayses.sav',clf)
+    #predict
     result = clf.predict(data_test)
-#score
+    #score
     return (accuracy_score(result, target_test))
+
